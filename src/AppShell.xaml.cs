956cc38f68c5f5970816e9d3d1aba4b7f5b95d62
Zach Hollis <zhollis@perfectserve.net>
2026-02-22 09:39:48 -0500

    Removing temp testing files

--- src/AppShell.xaml.cs
+++ src/AppShell.xaml.cs
@@ -3,12 +3,10 @@ namespace AniSprinkles;
 public partial class AppShell : Shell
 {
     private const string MediaDetailsRoute = "media-details";
-    private const string MediaDetailsSmokeRoute = "media-details-smoke";
 
     public AppShell()
     {
         InitializeComponent();
         Routing.RegisterRoute(MediaDetailsRoute, typeof(MediaDetailsPage));
-        Routing.RegisterRoute(MediaDetailsSmokeRoute, typeof(MediaDetailsSmokeTestPage));
     }
 }
--- src/Pages/MediaDetailsSmokeTestPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace AniSprinkles.Pages;
-
-public partial class MediaDetailsSmokeTestPage : ContentPage
-{
-    public MediaDetailsSmokeTestPage()
-    {
-        InitializeComponent();
-    }
-}
